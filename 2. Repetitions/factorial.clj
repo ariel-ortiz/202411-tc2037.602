4fc17f6e2986aa41ba1dc281122321633b10b602
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-02-22 13:07:27 -0600

    Add code to solve factorial using loop/recur and sequence API

--- 2. Repetitions/factorial.clj
+++ 2. Repetitions/factorial.clj
@@ -1,10 +1,25 @@
 (ns factorial)
 
-; Factorial version recursiva
+; Factorial versión recursiva
 (defn fact-v1
   [n]
   (if (zero? n)
     1
     (*' n (fact-v1 (dec n)))))
 
-(fact-v1 1000)
+; Factorial versión loop/recur (iterativa)
+(defn fact-v2
+  [n]
+  (loop [i 1
+         r 1]
+    (if (> i n)
+      r
+      (recur (inc i)
+             (*' i r)))))
+
+; Factorial versión API de secuencias
+(defn fact-v3
+  [n]
+  (reduce *' (range 2 (inc n))))
+
+(fact-v3 10000)
