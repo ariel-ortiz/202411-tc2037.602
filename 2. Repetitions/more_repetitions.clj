f8e96951cc6cb58dfb056f5d0835d9190e209a98
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-03-07 13:02:32 -0600

    Add code to solve problems 5, 6 and 10 of problem set 3

--- 2. Repetitions/more_repetitions.clj
+++ 2. Repetitions/more_repetitions.clj
@@ -47,6 +47,38 @@
   [s]
   (reduce #(insert %2 %1) () s))
 
+; Problema 5
+(defn binary
+  [n]
+  (loop [n n
+         r ()]
+    (if (zero? n)
+      r
+      (recur (quot n 2)
+             (cons (rem n 2) r)))))
+
+; Problema 6
+(defn prime-factors
+  [n]
+  (loop [n n
+         r []
+         i 2]
+    (cond
+      (= n 1)            (concat r ())
+
+      (zero? (rem n i))  (recur (quot n i)
+                                (conj r i)
+                                i)
+
+      :else              (recur n
+                                r
+                                (inc i)))))
+
+; Problema 10
+(defn pack
+  [s]
+  (partition-by identity s))
+
 (deftest test-expand
   (is (= () (expand ())))
   (is (= '(a) (expand '(a))))
@@ -66,5 +98,24 @@
          (insertion-sort '(4 3 6 8 3 0 9 1 7))))
   (is (= '(1 2 3 4 5 6) (insertion-sort '(1 2 3 4 5 6))))
   (is (= '(1 5 5 5 5 5 5) (insertion-sort '(5 5 5 1 5 5 5)))))
+
+(deftest test-binary
+  (is (= () (binary 0)))
+  (is (= '(1 1 1 1 0) (binary 30)))
+  (is (= '(1 0 1 1 0 0 0 0 0 1 0 0 0 0 1 1) (binary 45123))))
+
+(deftest test-prime-factors
+  (is (= () (prime-factors 1)))
+  (is (= '(2 3) (prime-factors 6)))
+  (is (= '(2 2 2 2 2 3) (prime-factors 96)))
+  (is (= '(97) (prime-factors 97)))
+  (is (= '(2 3 3 37) (prime-factors 666))))
+
+(deftest test-pack
+  (is (= () (pack ())))
+  (is (= '((a a a a) (b) (c c) (a a) (d) (e e e e))
+         (pack '(a a a a b c c a a d e e e e))))
+  (is (= '((1) (2) (3) (4) (5)) (pack '(1 2 3 4 5))))
+  (is (= '((9 9 9 9 9 9 9 9 9)) (pack '(9 9 9 9 9 9 9 9 9)))))
 
 (run-tests)
