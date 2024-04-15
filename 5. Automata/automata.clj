8e17fd47e986fd07507f7054efbdc56b508e32fc
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-04-15 12:17:04 -0600

    Add code to solve problems 2 and 3 of problem set 6

--- 5. Automata/automata.clj
+++ 5. Automata/automata.clj
@@ -34,4 +34,46 @@
   (is (not (accepts? dfa-1 "baa")))
   (is (not (accepts? dfa-1 "bbba"))))
 
+; Problema 2
+(def dfa-2 (->DFA :q0
+                  #{:q2}
+                  {:q0 {\0 :q1
+                        \1 :q3}
+                   :q1 {\0 :q1
+                        \1 :q2}
+                   :q2 {\0 :q1
+                        \1 :q2}
+                   :q3 {\0 :q3
+                        \1 :q3}}))
+
+(deftest test-problem2
+  (is (accepts? dfa-2 "01"))
+  (is (accepts? dfa-2 "0101"))
+  (is (accepts? dfa-2 "01111"))
+  (is (accepts? dfa-2 "000001"))
+  (is (not (accepts? dfa-2 "")))
+  (is (not (accepts? dfa-2 "00")))
+  (is (not (accepts? dfa-2 "1001011")))
+  (is (not (accepts? dfa-2 "1001010"))))
+
+; Problema 3
+(def dfa-3 (->DFA :q0 #{:q3} {:q0 {\x :q0
+                                   \y :q1}
+                              :q1 {\x :q0
+                                   \y :q2}
+                              :q2 {\x :q0
+                                   \y :q3}
+                              :q3 {\x :q3
+                                   \y :q3}}))
+
+(deftest test-problem3
+  (is (accepts? dfa-3 "yyy"))
+  (is (accepts? dfa-3 "xyxyyyx"))
+  (is (accepts? dfa-3 "xxxxxyyyyy"))
+  (is (accepts? dfa-3 "yyyxxxxyyy"))
+  (is (not (accepts? dfa-3 "")))
+  (is (not (accepts? dfa-3 "xxx")))
+  (is (not (accepts? dfa-3 "yxxyxxy")))
+  (is (not (accepts? dfa-3 "xyxyyxyyx"))))
+
 (run-tests)
