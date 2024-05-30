a7fb765363f6643be9a9ea85e0acd9fd6a58ed0e
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-05-30 12:57:11 -0600

    Add code to solve complete problem 1 of problem set 9

--- 8. Turing Machines/turing_machines.clj
+++ 8. Turing Machines/turing_machines.clj
@@ -16,11 +16,54 @@
 
 ; (println t)
 
+(defn make-tape
+  ([s]
+   (let [s (drop-while #(= % \_) s)]
+     (make-tape ""
+                (if (empty? s) \_ (first s))
+                (rest s))))
+  ([left head right]
+   (let [left (drop-while #(= % \_) left)
+         right (reverse (drop-while #(= % \_) (reverse right)))]
+     (->Tape (apply str left)
+             head
+             (apply str right)))))
+
+(defn write-tape
+  [{:keys [left right]} value]
+  (make-tape left value right))
+
+(defn shift-head
+  [{:keys [left head right]} direction]
+  (case direction
+    :right (make-tape (str left head)
+                      (or (first right) \_)
+                      (rest right))
+    :left (make-tape (or (butlast left) ())
+                     (or (last left) \_)
+                     (str head right))
+    (throw (ex-info (str "Bad direction: " direction) {}))))
+
+(defn accepts
+  [{:keys [initial-state accept-states transitions]} input]
+  (loop [tape (make-tape input)
+         current-state initial-state]
+    (if (contains? accept-states current-state)
+      (str tape)
+      (if-let [[write-symbol direction new-state]
+               ((transitions current-state) (.head tape))]
+        (recur (shift-head (write-tape tape write-symbol)
+                           direction)
+               new-state)
+        nil))))
 
 ; Problema 1
 (def tm-1 (->TM :q0
                 #{:q2}
-                {:q0 {\a [\a :right :q1]}
-                 :q1 {\_ [\_ :left :q2]}}))
+                {:q0 {\a [\a :right :q1]
+                      \_ [\_ :left :q2]}
+                 :q1 {\a [\a :right :q0]}}))
+
+(accepts tm-1 "aaaaaa")
 
 (run-tests)
